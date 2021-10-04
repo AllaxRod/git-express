db18162bba08b6ad621c7093b79b3d22793f0488
Allax <allaxbruno23@gmail.com>
2021-10-04 20:36:37 -0300

    Criando Servidor

--- git-express.js
+++ git-express.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const app = express();
+
+var usuarios = [
+    {
+        id:1,
+        name:'Away!'
+    },
+    {
+        id:2,
+        name:'Bora!'
+    }
+];
+app.get('/usuarios', (req, res) => {
+    res.send(usuarios);
+});
+
+app.listen(3000, () =>{
+    console.log('Ta rodando.')
+})
